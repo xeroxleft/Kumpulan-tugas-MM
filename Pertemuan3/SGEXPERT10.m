b7869d5aa834a56968303243dd44fc9d4364f10f
Ravli Ramadhan <43707122+xeroxleft@users.noreply.github.com>
2019-04-25 10:33:33 +0700

    Create SGEXPERT10.m

---
+++ Pertemuan3/SGEXPERT10.m
@@ -0,0 +1,10 @@
+%convert binary number
+%user input dan di convert oleh program
+
+biner = input('Input bilangan biner : ', 's');
+b = bin2dec(biner);
+disp('Hasil convert dari bilangan yang anda inputkan adalah');
+disp(b);
+
+
+
